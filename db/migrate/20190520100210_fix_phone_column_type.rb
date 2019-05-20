d8006b27359f5678284ae5511fce4d06a1d06100
Emeric Le Bail <emeric.lebail@gmail.com>
2019-05-20 13:54:10 +0200

    Rails new with frontend and form gems

---
+++ db/migrate/20190520100210_fix_phone_column_type.rb
@@ -0,0 +1,5 @@
+class FixPhoneColumnType < ActiveRecord::Migration[5.2]
+  def change
+    change_column :restaurants, :phone_number, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "La Fleche d'Argent", address: 'Paris', category: 'french', phone_number: '01 39 57 35 65')
+Restaurant.create(name: 'Loof', address: 'Singapour', category: 'chinese', phone_number: '+65 3456 2387')
+Restaurant.create(name: 'Hiro Dreams of Sushi', address: 'Tokyo', category: 'japanese', phone_number: '+65 3456 2387')
+Restaurant.create(name: 'Dodo La Saumure', address: 'Anvers', category: 'belgian', phone_number: '+34 56373 6935')
+Restaurant.create(name: 'Latteria', address: 'Singapour', category: 'italian', phone_number: '+65 5683 0075')
